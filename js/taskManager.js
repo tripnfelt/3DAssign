5c64277d670c45b55e0cb3be29b013401e1c84a3
Your Name <you@example.com>
2021-09-23 10:07:59 +1000

    method built

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,9 +1,22 @@
+//TaskManager Class
 class TaskManager {
   constructor(currentId = 0) {
     this.tasks = [];
-  
+    this.currentId = currentId;
   }
 }
-
+//test
 console.log(taskManager.tasks)
+
+//add task method
+const task = {
+  id: this.currentId++,
+  taskName: taskName,
+  comment: comment,
+  names: names,
+  dueDate: dueDate,
+  taskStatus: taskStatus,
+};
+
+this.tasks.push({ task });
 
